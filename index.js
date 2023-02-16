1b079a8feb0ea3865e037eadca6020034ccecd10
Mary <itsmaryyang@gmail.com>
2023-02-16 11:56:16 -0600

    create directory structure, file and folder names

---
+++ index.js
@@ -0,0 +1,16 @@
+// Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const jest = require("jest");
+
+// An array of questions for user input
+const questions = [];
+
+// A function to write HTML file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
