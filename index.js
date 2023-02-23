771c7a90a221c511f2b1758fa4831320ee0f4207
Mary <itsmaryyang@gmail.com>
2023-02-22 21:04:29 -0600

    Add helperHTML.js file

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
 
-// the full team Employee Data 
+// the full team Employee Data
 let employeeData = [];
 
 // An array of questions for user input
-const questions = async () => {
-  const data = await inquirer
+const questions = () => {
+  return inquirer
     .prompt([
       {
         type: "input",
@@ -30,8 +30,7 @@ const questions = async () => {
       },
       {
         type: "list",
-        message:
-          "Start by adding a Manager role, if none, proceed with other options",
+        message: "What is your role?",
         choices: ["Manager", "Engineer", "Intern"],
         name: "role",
       },
@@ -39,6 +38,7 @@ const questions = async () => {
     .then((data) => {
       console.log("-------------------------EMPLOYEE-------------------------");
       console.log(data);
+      const employee = new Employee(data.name, data.id, data.email);
       if (data.role === "Manager") {
         promptManager();
       } else if (data.role === "Engineer") {
@@ -52,26 +52,26 @@ const questions = async () => {
 };
 
 const promptManager = () => {
-  inquirer
+  return inquirer
     .prompt([
       {
         type: "input",
-        message: "What is the Manager's name?",
+        message: "Manager, what is your name?",
         name: "name",
       },
       {
         type: "input",
-        message: "What is the Manager's email?",
+        message: "Manager, what is your email?",
         name: "email",
       },
       {
         type: "input",
-        message: "What is the Manager's ID?",
+        message: "Manager, what is your ID?",
         name: "id",
       },
       {
         type: "input",
-        message: "What is the Manager office number?",
+        message: "Manager, what is your office number?",
         name: "officenumber",
       },
     ])
@@ -107,7 +107,7 @@ const menu = () => {
 };
 
 const addEngineer = () => {
-  inquirer
+  return inquirer
     .prompt([
       {
         type: "input",
@@ -145,7 +145,7 @@ const addEngineer = () => {
 };
 
 const addIntern = () => {
-  inquirer
+ return inquirer
     .prompt([
       {
         type: "input",
@@ -177,50 +177,4 @@ const addIntern = () => {
     });
 };
 
-// // A function to write HTML file
-// fs.writeFile(
-//   "./dist/index.html",
-//   `<!DOCTYPE html>
-//   <html lang="en">
-//     <head>
-//       <meta charset="UTF-8" />
-//       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-//       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//       <link rel="stylesheet" href="../assets/css/style.css" />
-//       <title>Nodejs Team Profile Generator</title>
-//       <link
-//         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
-//         rel="stylesheet"
-//         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
-//         crossorigin="anonymous"
-//       />
-//     </head>
 
-//     <body>
-//       <main>
-//         <div class="card" style="width: 18rem">
-//           <div class="card-body">
-//             <h5 class="card-title">Employee Name${data.name}</h5>
-//             <p class="card-text">
-//               Some quick example text
-//             </p>
-//           </div>
-//           <ul class="list-group list-group-flush">
-//             <li class="list-group-item">Emp ID:${data.id}</li>
-//             <li class="list-group-item">Email:${data.email}<a href="mailto:${data.email}></a></li>
-//             <li class="list-group-item">Role:${data.role}</li>
-//           </ul>
-//         </div>
-//       </main>
-
-//       <footer></footer>
-
-//       <script
-//         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
-//         integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD"
-//         crossorigin="anonymous"
-//       ></script>
-//     </body>
-//   </html>`,
-//   (err) => (err ? console.log(err) : console.log("Success!"))
-// );
---
+++ src/helperHTML.js
@@ -0,0 +1,48 @@
+// A function to write HTML file
+
+fs.writeFile(
+  "./dist/index.html",
+  `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="stylesheet" href="../assets/css/style.css" />
+      <title>Nodejs Team Profile Generator</title>
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+        crossorigin="anonymous"
+      />
+    </head>
+
+    <body>
+      <main>
+        <div class="card" style="width: 18rem">
+          <div class="card-body">
+            <h5 class="card-title">Employee Name${data.name}</h5>
+            <p class="card-text">
+              Some quick example text
+            </p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Emp ID:${data.id}</li>
+            <li class="list-group-item">Email:${data.email}<a href="mailto:${data.email}></a></li>
+            <li class="list-group-item">Role:${data.role}</li>
+          </ul>
+        </div>
+      </main>
+
+      <footer></footer>
+
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+        integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD"
+        crossorigin="anonymous"
+      ></script>
+    </body>
+  </html>`,
+  (err) => (err ? console.log(err) : console.log("Success!"))
+);
