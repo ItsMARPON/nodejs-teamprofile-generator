c7df481ca4f478a653d3c645b0fc35ac673d3946
Mary <itsmaryyang@gmail.com>
2023-02-22 18:55:06 -0600

    Updated inquirer prompt list for employee

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Include packages needed for this application
-const fs = require("fs");
+const fsPromises = require("fs").promises;
 const inquirer = require("inquirer");
 const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
 
-const employeeData = [];
+let employeeData = [];
 
 // An array of questions for user input
 const questions = [
@@ -27,7 +27,8 @@ const questions = [
   },
   {
     type: "list",
-    message: "What is your role?",
+    message:
+      "Start by adding a Manager role, if none, proceed with other options",
     choices: ["Manager", "Engineer", "Intern"],
     name: "role",
   },
@@ -78,6 +79,7 @@ const promptManager = () => {
       menu();
     });
 };
+
 const menu = () => {
   inquirer
     .prompt([
@@ -95,9 +97,11 @@ const menu = () => {
         addIntern();
       } else {
         process.exit();
+        //  need to add function to compile all employee data
       }
     });
 };
+
 const addEngineer = () => {
   inquirer
     .prompt([
@@ -135,6 +139,7 @@ const addEngineer = () => {
       menu();
     });
 };
+
 const addIntern = () => {
   inquirer
     .prompt([
@@ -163,57 +168,55 @@ const addIntern = () => {
       console.log(data);
       console.log("-------------------------INTERN-------------------------");
       const intern = new Intern(data.name, data.id, data.email, data.school);
-      employeeData.push(intern);
+      employeeData.push(data);
       menu();
     });
 };
+
 // // A function to write HTML file
+// fs.writeFile(
+//   "./dist/index.html",
+//   `<!DOCTYPE html>
+//   <html lang="en">
+//     <head>
+//       <meta charset="UTF-8" />
+//       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+//       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//       <link rel="stylesheet" href="../assets/css/style.css" />
+//       <title>Nodejs Team Profile Generator</title>
+//       <link
+//         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
+//         rel="stylesheet"
+//         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
+//         crossorigin="anonymous"
+//       />
+//     </head>
 
-employeeData.forEach((data) => {
-  fs.writeFile(
-    "./dist/index.html",
-    `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="stylesheet" href="../assets/css/style.css" />
-      <title>Nodejs Team Profile Generator</title>
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
-        crossorigin="anonymous"
-      />
-    </head>
+//     <body>
+//       <main>
+//         <div class="card" style="width: 18rem">
+//           <div class="card-body">
+//             <h5 class="card-title">Employee Name${gatherInfo.name}</h5>
+//             <p class="card-text">
+//               Some quick example text
+//             </p>
+//           </div>
+//           <ul class="list-group list-group-flush">
+//             <li class="list-group-item">Emp ID:${gatherInfo.id}</li>
+//             <li a href="#" class="list-group-item">Email:${gatherInfo.email}</a></li>
+//             <li class="list-group-item">Role:${gatherInfo.role}</li>
+//           </ul>
+//         </div>
+//       </main>
 
-    <body>
-      <main>
-        <div class="card" style="width: 18rem">
-          <div class="card-body">
-            <h5 class="card-title">Employee Name${data.name}</h5>
-            <p class="card-text">
-              Some quick example text
-            </p>
-          </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">Emp ID:${data.id}</li>
-            <li a href="#" class="list-group-item">Email:${data.email}</a></li>
-            <li class="list-group-item">Role:${data.role}</li>
-          </ul>
-        </div>
-      </main>
+//       <footer></footer>
 
-      <footer></footer>
-
-      <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
-        integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD"
-        crossorigin="anonymous"
-      ></script>
-    </body>
-  </html>`,
-    (err) => (err ? console.log(err) : console.log("Success!"))
-  );
-});
+//       <script
+//         src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+//         integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD"
+//         crossorigin="anonymous"
+//       ></script>
+//     </body>
+//   </html>`,
+//   (err) => (err ? console.log(err) : console.log("Success!"))
+// );
