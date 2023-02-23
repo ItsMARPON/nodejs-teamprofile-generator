996fa32bfd19bb94de2109a679b0cd54a1e7556b
Mary <itsmaryyang@gmail.com>
2023-02-22 19:47:03 -0600

    update Employee.test.js

--- test/employee.test.js
+++ test/employee.test.js
@@ -3,18 +3,29 @@ const fs = require("fs");
 
 jest.mock("fs");
 
+// gets ID from getId()
 describe("Employee", () => {
   describe("Initialization", () => {
-    it("should ", () => {
-      const employee = new Employee();
-      const file = "test.html";
-      let data;
+    test("should obtain employee Id", () => {
+      const employee = new Employee("Mary", 1234, "mary@gmail.com");
 
-      fs.readFileSync.mockReturnValue("Hello World!");
-      data = employee.read(file);
-
-      expect(data).toEqual("Hello World!");
-      expect(fs.readFileSync).lastCalledWith(file, "utf8");
+      expect(employee.getId()).toEqual(expect.any(Number));
     });
   });
+});
+
+// gets email from getEmail()
+test("should obtain employee email", () => {
+  const employee = new Employee("Mary", 1234, "mary@gmail.com");
+
+  expect(employee.getEmail()).toEqual(
+    expect.stringContaining(employee.email.toString())
+  );
+});
+
+// gets role from getRole()
+test("should obtain employee role", () => {
+  const employee = new Employee("Mary", 1234, "mary@gmail.com");
+
+  expect(employee.getRole()).toEqual("Employee");
 });
